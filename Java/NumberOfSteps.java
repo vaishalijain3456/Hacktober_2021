6c56ff83ac2be5558ef5b410a1a3f86944445aa9
Shreya Kaur <67049860+Evie241@users.noreply.github.com>
2021-10-22 23:47:45 +0530

    Create NumberOfSteps.java
    Pull Request for Issue #4 Staircase

---
+++ Java/NumberOfSteps.java
@@ -0,0 +1,20 @@
+public class NumberOfSteps {
+
+  public static int getSteps(int n)
+    {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return 1;
+        else if (n == 2)
+            return 2;
+        else
+            return getSteps(n - 3) + getSteps(n - 2) + getSteps(n - 1);
+    }
+ 
+    public static void main(String argc[])
+    {
+        int n = 7;
+        System.out.println(getSteps(n));
+    }
+}
